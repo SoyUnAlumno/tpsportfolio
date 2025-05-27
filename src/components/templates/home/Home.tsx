06e4519e9d6f048f67d0e990be49d69e95d7677c
tomps <tomaspraga@hotmail.es>
2025-05-27 22:28:27 +0200

    WIP installs tailwindcss

--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import Header from '@/components/cells/header/Header'
-
+import styles from './Home.module.css';
 function Home() {
     const { t } = useTranslation();
 
@@ -8,7 +8,7 @@ function Home() {
         <>
             <div>
                 <Header />
-                <p>
+                <p className={styles.p}>
                     {t('under_construction')} ~ {t('common.language.language')}
                 </p>
             </div>
